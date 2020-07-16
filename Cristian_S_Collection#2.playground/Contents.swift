3e462017402fc3005fa9345d1540e46a791b6452
TechyCriswiz <67975057+TechyCriswiz@users.noreply.github.com>
2020-07-16 13:58:17 -0500

    Add files via upload

---
+++ Cristian_S_Collection#2.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+var computerPartCollection: [String] = ["NVIDIA GeForce GTX 1080", "Intel i7-7700k", "GSkill Trident Z RGB", "ThermalTake Riing 12", "EVGA GQ 650", "Corsair H100i"]
+
+computerPartCollection.sort()
+print(computerPartCollection)
